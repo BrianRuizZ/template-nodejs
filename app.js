2002386089ff114fac2ae0a714d03a9eec2e4ca3
Brian Ruiz <98284315+BrianRuizZ@users.noreply.github.com>
2023-08-24 19:33:45 -0300

    Update app.js
    Signed-off-by: Brian Ruiz <98284315+BrianRuizZ@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
 const express = require('express');
-const { Pool } = require('pg');
-
 const app = express();
-const PORT = process.eventNames.PORT ?? 5432;
-const pool = new Pool({
-  user: 'fl0user',
-  host: 'postgres://fl0user:7RXJm6EOnMbi@ep-aged-bar-64615221.us-east-2.aws.neon.tech:5432/postgres?sslmode=require',
-  database: 'postgres',
-  password: '7RXJm6EOnMbi',
-  port: 5432,
-});
+const morgan = require('morgan')
 
-app.use(express.urlencoded({ extended: false }));
+// settings
 
-app.post('/guardar-datos', (req, res) => {
-  const { nombre, correo, mensaje } = req.body;
+app.set('port', process.env.PORT || 3000 );
+app.set('json spaces', 2)
 
-  const query = 'INSERT INTO datos_formulario (nombre, correo, mensaje) VALUES ($1, $2, $3)';
-  const values = [nombre, correo, mensaje];
+// middlewares
 
-  pool.query(query, values, (err, result) => {
-    if (err) {
-      console.error('Error al guardar los datos:', err);
-      return res.status(500).send('Error al guardar los datos.');
-    }
-    console.log('Datos guardados exitosamente');
-    return res.status(200).send('Datos guardados exitosamente.');
-  });
-});
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.listen(PORT, () => {
-  console.log(`Servidor Node.js corriendo en https://template-nodejs-3cy1-dev.fl0.io/`);
+// routes
+
+app.use(require('./routes/index'));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
